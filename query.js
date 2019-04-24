dc683772af1b9c5f9f1893399c6a1b7c4769d603
Carl Durand <carldurand.student@careerdevs.com>
2019-04-24 00:35:34 +0000

    Use jQuery to Modify the Entire Page

--- query.js
+++ query.js
@@ -14,7 +14,7 @@ $("body").addClass("animated hinge");
   });
 </script>
 
-<!-- Only change code above this line. -->
+Only change code above this line.
 
 <div class="container-fluid">
   <h3 class="text-primary text-center">jQuery Playground</h3>
